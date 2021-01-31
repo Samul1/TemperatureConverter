c5b22042eb04146e9ee8c45229c6d07180bf7679
Samuli Juutinen <juutinen.samuli@gmail.com>
2021-01-31 12:15:07 +0200

    Add project files.

---
+++ LampotilaMuunnin/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace LampotilaMuunnin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBoxAnswer = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtBoxAnswer
+            // 
+            this.txtBoxAnswer.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxAnswer.Location = new System.Drawing.Point(12, 327);
+            this.txtBoxAnswer.Multiline = true;
+            this.txtBoxAnswer.Name = "txtBoxAnswer";
+            this.txtBoxAnswer.ReadOnly = true;
+            this.txtBoxAnswer.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtBoxAnswer.Size = new System.Drawing.Size(397, 120);
+            this.txtBoxAnswer.TabIndex = 6;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(114, 496);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(173, 45);
+            this.btnConvert.TabIndex = 7;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(288, 235);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(288, 280);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 236);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(195, 20);
+            this.textBox1.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(251, 283);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "TO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(244, 238);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "FROM";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::LampotilaMuunnin.Properties.Resources.titlePicLampo;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(397, 200);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(421, 577);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtBoxAnswer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Temperature Converter";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtBoxAnswer;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ LampotilaMuunnin/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+/// @author Samul1
+/// version 31.01.2021
+/// 
+/// <summary>
+/// Program will convert temperature.
+/// Celcius - Fahrenheit - Kelvin
+/// </summary>
+namespace LampotilaMuunnin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // create a text array for your comboBoxes
+            string[] a = { "Celcius", "Fahrenheit", "Kelvin" }; 
+            string[] b = { "Fahrenheit", "Celcius", "Kelvin" };
+            // adds text from arrays to the comboBoxes
+            comboBox1.DataSource = a;
+            comboBox2.DataSource = b;
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            double startingTemp, resultTemp;
+            // sta
+            startingTemp = double.Parse(textBox1.Text);
+            // Converting Celcius to Fahrenheit
+            if (comboBox1.Text == "Celcius" && comboBox2.Text == "Fahrenheit")
+            {
+                resultTemp = ((startingTemp * 1.8) + 32);
+                txtBoxAnswer.Text = resultTemp.ToString("0.0" + " °F");
+            }
+            // Converting Kelvin to Fahrenheit
+            if (comboBox1.Text == "Kelvin" && comboBox2.Text == "Fahrenheit")
+            {
+                resultTemp = ((startingTemp * 1.8) - 459.67);
+                txtBoxAnswer.Text = resultTemp.ToString("0.0" + " °F");
+            }
+            // Converting Fahrenheit to Celcius
+            if (comboBox1.Text == "Fahrenheit" && comboBox2.Text == "Celcius")
+            {
+                resultTemp = ((startingTemp - 32) / 1.8);
+                txtBoxAnswer.Text = resultTemp.ToString("0.0" + " °C");
+            }
+            // Converting Kelvin to Celcius
+            if (comboBox1.Text == "Kelvin" && comboBox2.Text == "Celcius")
+            {
+                resultTemp = (startingTemp - 273.15);
+                txtBoxAnswer.Text = resultTemp.ToString("0.0" + " °C");
+            }
+            // Converting Celcius to Kelvin
+            if (comboBox1.Text == "Celcius" && comboBox2.Text == "Kelvin")
+            {
+                resultTemp = (startingTemp + 273.15);
+                txtBoxAnswer.Text = resultTemp.ToString("0.0" + " K");
+            }
+            // Converting Fahrenheit to Kelvin
+            if (comboBox1.Text == "Fahrenheit" && comboBox2.Text == "Kelvin")
+            {
+                resultTemp = ((startingTemp + 459.67) / 1.8);
+                txtBoxAnswer.Text = resultTemp.ToString("0.0" + " K");
+            }
+        }
+    }
+}
